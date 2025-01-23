479e98725f9cfce68d933f6a975c8b9805425027
Specs <464089419@qq.com>
2025-01-23 14:31:49 +0800

    author avator position

--- resources/views/web/author/show.blade.php
+++ resources/views/web/author/show.blade.php
@@ -7,24 +7,21 @@
 
 @section('content')
 <div class="card py-8 mb-8">
-    <div class="flex items-start">
-        @if($author->pic)
-        <img class="w-20 h-auto rounded-md mr-4" src="{{ $author->pic }}" alt="{{ $author->name }}">
-        @endif
-        <div>
-            <h2 class="text-lg mb-3">
-                <a class="link" href="{{ route('author.show', $author->author_id) }}">@if($author->dynasty)【{{ $author->dynasty->name }}】@endif{{ $author->name }}</a>
-            </h2>
-            <div class="author-content escape-html leading-10 [&>p]:mb-6">
-                {!! $author->content !!} 
-                <!-- @if($author->books_count > 0)
+    @if($author->pic)
+    <img class="w-20 h-auto rounded-md mr-4 float-left" src="{{ $author->pic }}" alt="{{ $author->name }}">
+    @endif
+
+    <h2 class="text-lg mb-3">
+        <a class="link" href="{{ route('author.show', $author->author_id) }}">@if($author->dynasty)【{{ $author->dynasty->name }}】@endif{{ $author->name }}</a>
+    </h2>
+    <div class="author-content escape-html leading-10 [&>p]:mb-6">
+        {!! $author->content !!}
+        <!-- @if($author->books_count > 0)
                 <a href="{{ route('book.index', ['author_id' => $author->author_id]) }}" class="link primary">&raquo; {{ $author->books_count }}部作品</a>
                 @endif -->
-                @if($author->poems_count > 0)
-                <a href="{{ route('poem.index', ['author_id' => $author->author_id]) }}" class="link primary">&raquo; {{ $author->poems_count }}首诗词</a>
-                @endif
-            </div>
-        </div>
+        @if($author->poems_count > 0)
+        <a href="{{ route('poem.index', ['author_id' => $author->author_id]) }}" class="link primary">&raquo; {{ $author->poems_count }}首诗词</a>
+        @endif
     </div>
 </div>
 
