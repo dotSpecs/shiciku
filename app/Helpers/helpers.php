31a2988b2b73dbd3a9d9900cc8804b4bd9fa8d19
Specs <464089419@qq.com>
2025-09-17 17:22:42 +0800

    诗词slug

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -9,5 +9,7 @@ function poem_slug(Poem $poem)
 
 function name2slug($name)
 {
-    return str_replace([' ', '/', '-'], ['', '_', '_'], $name);
+    $name = str_replace([' ', '/', '-'], ['', '_', '_'], $name);
+
+    return mb_substr($name, 0, 60);
 }
