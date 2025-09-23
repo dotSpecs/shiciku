db70a7712ca6bc4b002938a3e8f3b044bc70f82d
Specs <464089419@qq.com>
2025-09-23 09:45:31 +0800

    ad

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -75,10 +75,10 @@
                 @yield('sidebar')
 
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6074386496019881" crossorigin="anonymous"></script>
-                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6074386496019881" data-ad-slot="6822112578" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                {{-- <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6074386496019881" data-ad-slot="6822112578" data-ad-format="auto" data-full-width-responsive="true"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                </script> --}}
             </div>
         </div>
     </div>
