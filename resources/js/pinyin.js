6ebce283c9880dbd0f2f7a597d77f9c156fe2ade
Specs <464089419@qq.com>
2026-01-30 11:45:16 +0800

    pinyin

--- resources/js/pinyin.js
+++ resources/js/pinyin.js
@@ -6,5 +6,20 @@ window.pinyinPro = {
     customPinyin,
 };
 
+window.pinyinPro.customPinyin({
+    将进酒: "qiāng jìn jiǔ",
+    夕阳斜: "xī yáng xiá",
+    万竿斜: "wàn gān xiá",
+    春风拂槛: "chūn fēng fú jiàn",
+    槛外: "jiàn wài",
+    鬓毛衰: "bìn máo cuī",
+    朝如青丝: "zhāo rú qīng sī",
+    天姥: "tiān mǔ",
+    泪不乾: "lèi bù gān",
+    重叠: "chóng dié",
+    曲项: "qū xiàng",
+    长相: "cháng xiāng",
+});
+
 // 导出以确保代码被执行
 export default window.pinyinPro;
